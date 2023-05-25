481a852201d759c6c57e1a499fa29e5b1127bd54
Deven Jain <83266889+SpaceMonkey6000@users.noreply.github.com>
2023-05-25 20:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ st.title("🤖 StackWalls AI")
 prompt = st.text_area("How may we help you today?", height=40)#, max_chars=100)
 
 title_template = PromptTemplate(
-    input_variables = ['req'],
-    template='Based on the requirements given to you, come up with tools that can do the required tasks and also get the details of the number of pages/screens required in the project. The type of project may include: App/Website/UI-UX/Design. The platforms used may be: Figma/Shopify/Wordpress/Kajabi/Canva. You are to return a list of the format: <project type>,<Name of the Business/name of the user>, <Best platform availiable to complete the task>, <Details of the pages/screens required in the project>. Here are the requirements: {req}'
+    input_variables = ['topic'],
+    template='Based on the requirements given to you, come up with tools that can do the required tasks and also get the details of the number of pages/screens required in the project. The type of project may include: App/Website/UI-UX/Design. The platforms used may be: Figma/Shopify/Wordpress/Kajabi/Canva. You are to return a list of the format: <project type>,<Name of the Business/name of the user>, <Best platform availiable to complete the task>, <Details of the pages/screens required in the project>. Here are the requirements: {topic}'
 )
 
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
 
 if prompt:
-    response = title_chain.run(req=prompt)
+    response = title_chain.run(topic=prompt)
     st.write(response)
